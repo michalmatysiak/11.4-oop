82521699cf6cac3737ccf1930bba148b1788e47e
Michał Matysiak <michalmatysiak02@gmail.com>
2018-03-28 10:56:32 +0200

    Add more prototype

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function Phone(brand, price, color) {
+function Phone(brand, price, color,benchmark) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
+	this.benchmark = benchmark;
+	this.year = year;
 }
 Phone.prototype.printInfo = function () {
-	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + ' $.')
+	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + ' $.' + '. The phone is' + this.benchmark + ' benchmark points.')
 }
-var iPhone6S = new Phone('Aplle',2250,'silver')
+var iPhone6S = new Phone('Aplle',2250,'silver', 23032)
 iPhone6S.printInfo();
-var SamsungGalaxyS6 = new Phone('Samsung',2550,'black')
+var SamsungGalaxyS6 = new Phone('Samsung',2550,'black',43555)
 SamsungGalaxyS6.printInfo();
-var OnePlusOne = new Phone('OnePlus',1980,'red')
+var OnePlusOne = new Phone('OnePlus',1980,'red',65332)
 OnePlusOne.printInfo();

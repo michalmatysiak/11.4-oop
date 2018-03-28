467ed1dfadff59f74f5c727c2a65c117270c7ff0
Michał Matysiak <michalmatysiak02@gmail.com>
2018-03-28 10:57:40 +0200

    Add next prototype

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function Phone(brand, price, color,benchmark) {
+function Phone(brand, price, color,benchmark,year) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
@@ -6,11 +6,11 @@ function Phone(brand, price, color,benchmark) {
 	this.year = year;
 }
 Phone.prototype.printInfo = function () {
-	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + ' $.' + '. The phone is' + this.benchmark + ' benchmark points.')
+	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + ' $.' + 'The phone is ' + this.benchmark + ' benchmark points ' + 'and was released on the market in ' + this.year + '.')
 }
-var iPhone6S = new Phone('Aplle',2250,'silver', 23032)
+var iPhone6S = new Phone('Aplle',2250,'silver', 23032,2016)
 iPhone6S.printInfo();
-var SamsungGalaxyS6 = new Phone('Samsung',2550,'black',43555)
+var SamsungGalaxyS6 = new Phone('Samsung',2550,'black',43555,2017)
 SamsungGalaxyS6.printInfo();
-var OnePlusOne = new Phone('OnePlus',1980,'red',65332)
+var OnePlusOne = new Phone('OnePlus',1980,'red',65332,2018)
 OnePlusOne.printInfo();
